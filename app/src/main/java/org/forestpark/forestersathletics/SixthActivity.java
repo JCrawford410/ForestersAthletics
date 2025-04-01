711a986d4e06fa3ed4391973bb309b49781a276b
JCrawford <Resiantz@gmail.com>
2025-04-01 07:44:30 -0400

    3/27/2025 3/31/2025 4/1/2025

--- app/src/main/java/org/forestpark/forestersathletics/SixthActivity.java
+++ app/src/main/java/org/forestpark/forestersathletics/SixthActivity.java
@@ -13,19 +13,42 @@ public class SixthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sixth);
 
-
+        Button badmintonButton = findViewById(R.id.button_badminton);
+        Button baseballButton = findViewById(R.id.button_baseball);
         Button basketballButton = findViewById(R.id.button_basketball);
-        Button soccerButton = findViewById(R.id.button_soccer);
         Button footballButton = findViewById(R.id.button_football);
+        Button lacrosseButton = findViewById(R.id.button_lacrosse);
+        Button soccerButton = findViewById(R.id.button_soccer);
+        Button softballButton = findViewById(R.id.button_softball);
+        Button swimmingButton = findViewById(R.id.button_swimming);
+        Button tennisButton = findViewById(R.id.button_tennis);
+        Button trackButton = findViewById(R.id.button_track);
+        Button volleyballButton = findViewById(R.id.button_volleyball);
+        Button wrestlingButton = findViewById(R.id.button_wrestling);
 
+        badmintonButton.setOnClickListener(v -> openSecondActivity("Badminton"));
+
+        baseballButton.setOnClickListener(v -> openSecondActivity("Baseball"));
 
         basketballButton.setOnClickListener(v -> openSecondActivity("Basketball"));
 
-        soccerButton.setOnClickListener(v -> openSecondActivity("Soccer"));
+        lacrosseButton.setOnClickListener(v -> openSecondActivity("Lacrosse"));
 
         footballButton.setOnClickListener(v -> openSecondActivity("Football"));
 
+        soccerButton.setOnClickListener(v -> openSecondActivity("Soccer"));
 
+        softballButton.setOnClickListener(v -> openSecondActivity("Softball"));
+
+        swimmingButton.setOnClickListener(v -> openSecondActivity("Swimming"));
+
+        tennisButton.setOnClickListener(v -> openSecondActivity("Tennis"));
+
+        trackButton.setOnClickListener(v -> openSecondActivity("Track"));
+
+        volleyballButton.setOnClickListener(v -> openSecondActivity("Volleyball"));
+
+        wrestlingButton.setOnClickListener(v -> openSecondActivity("Wrestling"));
     }
     private void openSecondActivity(String sport) {
         Intent intent = new Intent(SixthActivity.this, SecondActivity.class);
